6393503fd86e5417af0b94e91b3eb9e09a0cbb76
kbatuigas <36839689+kbatuigas@users.noreply.github.com>
2020-07-24 15:20:24 -0400

    Add blog

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,29 @@
 module.exports = {
+    siteMetadata: {
+        // Used for the site title and SEO
+        title: `Kat Batuigas`,
+        // Used to provide alt text for your avatar
+        author: `Kat Batuigas`,
+        // Used for SEO
+        description: `Personal site`,
+        // Used for resolving images in social cards
+        siteUrl: `https://kbatuigas-workshop-101-site.netlify.app/`,
+        // Used for social links in the root footer
+        social: [
+            {
+                name: `Twitter`,
+                url: `https://twitter.com/alsokrista`,
+            },
+        ],
+    },
     plugins: [
-        `gatsby-plugin-mdx`
+        `gatsby-plugin-mdx`,
+        {
+            resolve: `gatsby-theme-blog`,
+            options: {
+                basePath: `/blog`,
+                mdxOtherwiseConfigured: true
+            }
+        }
     ]
 }
