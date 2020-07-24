7a8aeeaca8ae072b8788bcadda61550b77250008
kbatuigas <36839689+kbatuigas@users.noreply.github.com>
2020-07-24 15:39:37 -0400

    Customized biocontent

---
+++ src/gatsby-theme-blog/components/bio-content.js
@@ -0,0 +1,16 @@
+import React, { Fragment } from "react"
+import { Styled } from "theme-ui"
+
+/**
+ * Shadow me to add your own bio content
+ */
+
+const BioContent = () => (
+  <Fragment>
+    Words by <Styled.a href="/">Kat Batuigas</Styled.a>.
+    <br />
+    Words are hard tho
+  </Fragment>
+)
+
+export default BioContent
